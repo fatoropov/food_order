6f7b0411c6deda33d06e82200a5d42b0e3513f79
Fedor Toropov <f.a.toropov@mail.ru>
2023-04-29 18:27:54 +0500

    Создал модели

--- food_order/models.py
+++ food_order/models.py
@@ -5,4 +5,51 @@ class Employee(models.Model):
     """ Модель сотрудника """
 
     name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30)
     department = models.CharField(max_length=25)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Dish(models.Model):
+    """ Модель блюда """
+
+    name = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=50,
+                            unique=True)
+    image = models.ImageField(upload_to='dish/%Y/%m/%d',
+                              blank=True)
+    description = models.TextField(blank=True)
+    composition = models.CharField(max_length=250)
+    price = models.DecimalField(max_digits=10,
+                                decimal_places=2)
+    available = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created']),
+        ]
+
+    def __str__(self):
+        return self.name
+
+
+class Order(models.Model):
+    """ Модель заказа """
+
+    employee = models.ForeignKey(Employee,
+                                 related_name='orders',
+                                 on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_created=True)
